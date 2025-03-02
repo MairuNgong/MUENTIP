c93a9699ea081314fa74abf92f0ee3e7c707cbfa
sgissweet <nitkamon.67908@gmail.com>
2025-03-02 21:55:33 +0700

    home use top most 5 tags done

--- MUENTIP/Controllers/HomeController.cs
+++ MUENTIP/Controllers/HomeController.cs
@@ -13,36 +13,41 @@ namespace MVC_test.Controllers
         {
             _context = context;
         }
-        // new push
+
         public IActionResult Index()
         {
             var activityFromDb = _context.Activities
-                                .OrderByDescending(t => t.ActivityId) 
-                                .Select(t => new ActivityCardViewModel
-            {
-                ActivityId = t.ActivityId,
-                Title = t.Title,
-                Owner = t.User.UserName,
-                Location = t.Location,
-                PostDateTime = t.PostDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                StartDateTime = t.StartDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                EndDateTime = t.EndDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                DeadlineDateTime = t.DeadlineDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                ApplyMax = t.ApplyMax,
-                ApplyCount = t.Applications.Count(),
-                TagsList = t.ActivityTags != null ? t.ActivityTags.Select(at => at.Tag.TagName).ToList() : new List<string>(),
-                Description = t.Description
-            }).ToList();
+                .OrderByDescending(t => t.ActivityId)
+                .Select(t => new ActivityCardViewModel
+                {
+                    ActivityId = t.ActivityId,
+                    Title = t.Title,
+                    Owner = t.User.UserName,
+                    Location = t.Location,
+                    PostDateTime = t.PostDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    StartDateTime = t.StartDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    EndDateTime = t.EndDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    DeadlineDateTime = t.DeadlineDateTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    ApplyMax = t.ApplyMax,
+                    ApplyCount = t.Applications.Count(),
+                    TagsList = t.ActivityTags != null ? t.ActivityTags.Select(at => at.Tag.TagName).ToList() : new List<string>(),
+                    Description = t.Description
+                }).ToList();
 
-            var tagsFromDb = _context.Tags
-                .Select(t => new TagFilterViewModel { TagName = t.TagName })
+            var topTags = _context.ActivityTypes
+                .GroupBy(at => at.Tag.TagName)
+                .Select(g => new { TagName = g.Key, Count = g.Count() })
+                .OrderByDescending(g => g.Count)
+                .Take(6)
+                .Select(g => new TagFilterViewModel { TagName = g.TagName })
                 .ToList();
 
             var model = new HomeViewModel
             {
                 Cards = activityFromDb,
-                Tags = tagsFromDb
+                Tags = topTags 
             };
+
             return View(model);
         }
     }
